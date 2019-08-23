f2787005acc470d409d5cca84cf774fd189eeef2
Rajaputra <crajaputra@deloitte.com>
2019-08-23 15:31:26 -0400

    byeeeeeeeeee

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 3000;
 
 var routes = require('./api/routes');
 routes(app);
-app.get('/test/routes' , (req, res) => res.send("hello, world"));
+app.get('/test/routes' , (req, res) => res.send("bye, world"));
 app.listen(port, function() {
    console.log('Server started on port: ' + port);
 });
